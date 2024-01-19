9e67dec2761a0f2f50487f0fcdbf2316f86308a5
JCovi <joshpcovington@gmail.com>
2024-01-18 20:04:01 -0700

    bugs

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,12 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const databaseUrl = process.env.DATABASE_URL || process.env.LOCAL_DATABASE_URL;
+
+const sequelize = new Sequelize(databaseUrl, {
   dialect: 'mysql',
   protocol: 'mysql',
   port: 3306,
 });
 
 module.exports = sequelize;
-
