04d28e0c6dda7d9def124ecce2cd153309e23e87
JCovi <joshpcovington@gmail.com>
2024-01-19 16:57:04 -0700

    .

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,21 @@
-const { Sequelize } = require('sequelize');
+const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const databaseUrl = process.env.DATABASE_URL || process.env.LOCAL_DATABASE_URL;
+let sequelize;
 
-const sequelize = new Sequelize(databaseUrl, {
-  dialect: 'mysql',
-  protocol: 'mysql',
-  port: 3306,
-});
+if (process.env.JAWSDB_URL) {
+sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
 
-module.exports = sequelize;
+ sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    }
+  );
+}
+  module.exports = sequelize;
