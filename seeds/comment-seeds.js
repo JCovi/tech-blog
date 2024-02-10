a48f45111166c22f93498b74027d4594f475920f
JCovi <joshpcovington@gmail.com>
2024-02-10 14:07:40 -0700

    fixed comment_date field in comment-seeds.js

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -2,26 +2,25 @@ const { Comment } = require('../models');
 
 const commentData = [
   {
-    "text": "Good Read",
-    "comment_date": "January 10th 2024",
-    "user_id": 3,
-    "post_id": 2
+    text: "Good Read",
+    comment_date: new Date("2024-01-10T00:00:00"),
+    user_id: 3,
+    post_id: 2
   },
   {
-    "text": "keep it up",
-    "comment_date": "January 20th 2024",
-    "user_id": 1,
-    "post_id": 1
+    text: "keep it up",
+    comment_date: new Date("2024-01-20T00:00:00"),
+    user_id: 1,
+    post_id: 1
   },
   {
-    "text": "Awesome Read",
-    "comment_date": "January 12th 2024",
-    "user_id": 2,
-    "post_id": 1
+    text: "Awesome Read",
+    comment_date: new Date("2024-01-12T00:00:00"),
+    user_id: 2,
+    post_id: 1
   },
-]
+];
 
 const seedComments = () => Comment.bulkCreate(commentData);
 
 module.exports = seedComments;
-
